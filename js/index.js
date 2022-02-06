f40d2d4081b155467c0942bfccfab14622c6ab00
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-02-06 15:51:15 +0300

    feature: incorporated js- added js functions

--- js/index.js
+++ js/index.js
@@ -1,11 +1,19 @@
 
-function myButton() {
-    document.write("submitted")
-    var result = 0;
-    var correctAnswers = document.one.question.value;
-    if (currentAnswer == true) {correct++};
-    document.write(correct)
+// function myButton() {
+//     document.write("submitted")
+//     var result = 0;
+//     var correctAnswers = [{}];
+//     if (currentAnswer == true) {correct++};
+//     document.write(result)
         
 
         
+// }
+function Button() {
+    var result = 0;
+    var moja = document.que.question.value;
+    var mbili = document.que.question1.value;
+    var tatu = document.que.question2.value;
+    var nne = document.que.question3.value;
+    var tano = document.que.question4.value;
 }
